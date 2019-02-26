9736ab630727f038cb4a40223a5f983a04ae7f6d
Karn Gulati <karngulati@gmail.com>
2019-02-26 23:29:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
-function deleteFromObjectByKey(object, key) {
+function ddestructivelyDeleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object)
   delete newObj[key]
   return newObj
